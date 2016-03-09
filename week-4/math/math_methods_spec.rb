fb5ba1f52edb0dddcfcf8ef923951dd98f730562
Caitlin Hoffman <choff999@gmail.com>
2016-03-08 16:11:02 -0800

    Create errors file

--- week-4/math/math_methods_spec.rb
+++ week-4/math/math_methods_spec.rb
@@ -39,4 +39,9 @@ describe "divide" do
     expect(divide(10,2)).to eq(5.0)
   end
 end
-
+How do you define a local variable?
+How do you define a method?
+What is the difference between a local variable and a method?
+How do you run a ruby program from the command line?
+How do you run an RSpec file from the command line?
+What was confusing about this material? What made sense?
