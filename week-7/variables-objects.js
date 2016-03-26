2f65007daa816d4dea7821e19e1a1ef07eb273ad
Caitlin Hoffman <choff999@gmail.com>
2016-03-25 20:25:58 -0400

    Initial Commit

--- week-7/variables-objects.js
+++ week-7/variables-objects.js
@@ -5,7 +5,7 @@
 // __________________________________________
 // Write your code below.
 
-var secretNumber = 
+var secretNumber = 7
 var password = "just open the door"
 var allowedIn = false
 var members = ["John", "Mary"]
